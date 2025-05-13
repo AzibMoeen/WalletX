dbf264a2122a2233bd657beb1d4a3bded1cdefff
AzibMoeen <azibmaher771@gmail.com>
2025-05-13 17:05:06 +0500

    feat: Add Vercel Analytics for improved tracking and remove unused gradient button component

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./global2.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({ children }) {
       >
         <ThemeProvider>
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
